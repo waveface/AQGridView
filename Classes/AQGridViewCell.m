39ed2fcecca12cbb94e48df2efd8bf625358f191
Evadne Wu <ev@monoceroi.com>
2011-03-24 00:22:52 +0800

    works on the code so there are no warnings

--- Classes/AQGridViewCell.m
+++ Classes/AQGridViewCell.m
@@ -317,37 +317,13 @@
 	if ( (_cellFlags.usingDefaultSelectedBackgroundView == 1) && (_selectedBackgroundView == nil) )
 	{
 	
-		NSString * imageName;
-	
 #ifdef BUILTIN_IMAGES
 		unsigned char * pngBytes = AQGridSelection_png;
 		NSUInteger pngLength = AQGridSelection_png_len;
-		switch ( _cellFlags.selectionStyle )
-		{
-			case AQGridViewCellSelectionStyleBlue:
-			default:
-				break;
-				
-			case AQGridViewCellSelectionStyleGray:
-				imageName = @"AQGridSelectionGray.png";
-				break;
-				
-			case AQGridViewCellSelectionStyleBlueGray:
-				imageName = @"AQGridSelectionGrayBlue.png";
-				break;
-				
-			case AQGridViewCellSelectionStyleGreen:
-				imageName = @"AQGridSelectionGreen.png";
-				break;
-				
-			case AQGridViewCellSelectionStyleRed:
-				imageName = @"AQGridSelectionRed.png";
-				break;
-		}
-		
 		NSData *pngData = [NSData dataWithBytesNoCopy: pngBytes length: pngLength freeWhenDone: NO];
 		_selectedBackgroundView = [[UIImageView alloc] initWithImage: [UIImage imageWithData: pngData]];
 #else
+		NSString * imageName;
 		imageName = @"AQGridSelection.png";
 		switch ( _cellFlags.selectionStyle )
 		{
